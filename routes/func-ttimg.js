11e61dc3d039c14137fc77a37789a23813261a9e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-26 17:06:11 -0600

    Create func-ttimg.js

---
+++ routes/func-ttimg.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const ttimg = async (link) => {
+    try {    
+        let url = `https://dlpanda.com/es?url=${link}&token=G7eRpMaa`;    
+        let response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        let imgSrc = [];
+        $('div.col-md-12 > img').each((index, element) => {
+            imgSrc.push($(element).attr('src'));
+        });
+        if (imgSrc.length === 0) {
+            return { data: '*[❗] No se encontraron imágenes en el enlace proporcionado.*' };
+        }
+        return { data: imgSrc }; 
+    } catch (error) {
+        console.log(error);
+        return { data: '*[❗] No se obtuvo respuesta de la página, intente más tarde.*'};
+    }
+};
+
+module.exports = ttimg;
