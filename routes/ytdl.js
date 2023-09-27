c0793e2e86b3371daa6a3bf37619c043759fc8a7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-26 22:15:58 -0600

    Update ytdl.js

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -5,14 +5,14 @@ const cheerio = require('cheerio');
 const { rateLimit } = require('express-rate-limit');
 const apicache = require('apicache');
 const fs = require('fs');
-const YT = require('./func/YT_mp3_mp4'); 
+const YT = require('./func/YT_mp3_mp4');
 
 const app = express();
 
 const apiRequestLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 40,
-  handler: function (req, res) {
+  handler: function(req, res) {
     return res.status(429).json({ error: 'Rate limit exceeded' });
   },
 });
@@ -26,37 +26,21 @@ router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
   res.header('X-XSS-Protection', '1; mode=block');
   res.header('X-Content-Type-Options', 'nosniff');
   res.header('Strict-Transport-Security', 'max-age=63072000');
-  res.setHeader('Content-Type', 'application/json');
-  
+  res.setHeader('Content-Type', 'audio/mpeg');
+
   const match_url = req.query.url;
-  
+
   try {
     const audioData = await YT.mp3(match_url);
-    const jsonResponse = {
-      meta: {
-        channelUrl: audioData.meta.channelUrl,
-        views: audioData.meta.views,
-        category: audioData.meta.category,
-        id: audioData.meta.id,
-        url: audioData.meta.url,
-        publicDate: audioData.meta.publicDate,
-        uploadDate: audioData.meta.uploadDate,
-        keywords: audioData.meta.keywords,
-        title: audioData.meta.title,
-        channel: audioData.meta.channel,
-        seconds: audioData.meta.seconds,
-        image: audioData.meta.image,
-      },
-      path: audioData.path,
-      size: fs.statSync(audioData.path).size,
-    };
-    res.send(JSON.stringify(jsonResponse, null, 4));
+    const fileStream = fs.createReadStream(audioData.path);
+    fileStream.pipe(res);
   } catch (error) {
     if (!error.response) {
-      res.json({ error: 'An error occurred' });
+      res.status(500).send('An error occurred');
     } else {
-      res.json({ error: 'An error occurred' });
+      res.status(500).send('An error occurred');
     }
   }
 });
+
 module.exports = router;
