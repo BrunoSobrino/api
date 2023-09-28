0a747d973c611338f8f02bb8dbc4b44158f45abc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-27 21:41:06 -0600

    Create ytmp3_2.js

---
+++ routes/ytmp3_2.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { rateLimit } = require('express-rate-limit');
+const apicache = require('apicache');
+const YT = require('./func/YT_mp3_mp4');
+
+const app = express();
+
+const apiRequestLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 40,
+  handler: function(req, res) {
+    return res.status(429).json({ error: 'Rate limit exceeded' });
+  },
+});
+
+const cache = apicache.middleware;
+
+router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
+  res.setHeader('Content-Type', 'audio/mpeg');
+
+  const match_url = req.query.url;
+
+  try {
+    const audioBuffer = await YT.mp3_2(match_url);
+    res.end(audioBuffer);
+  } catch (error) {
+    if (!error.response) {
+      res.status(500).send('An error occurred');
+    } else {
+      res.status(500).send('An error occurred');
+    }
+  }
+});
+
+module.exports = router;
