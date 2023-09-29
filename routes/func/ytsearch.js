8bb330078be12082d8f45d6c31397aea46c17a15
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 00:28:51 -0600

    Create ytsearch.js

---
+++ routes/func/ytsearch.js
@@ -0,0 +1,26 @@
+const yts = require('yt-search');
+
+const ytsearch = async (searchText) => {
+  if (!searchText) {
+    throw new Error('Texto de búsqueda no especificado');
+  }
+
+  const results = await yts(searchText);
+
+  const formattedResults = results.all.map((video) => {
+    if (video.type === 'video') {
+      return {
+        title: video.title,
+        url: video.url,
+        duration: video.timestamp,
+        uploaded: video.ago,
+        views: video.views,
+      };
+    }
+    return null;
+  }).filter(Boolean);
+
+  return formattedResults;
+};
+
+module.exports = { ytsearch };
