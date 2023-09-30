ef81658741c9363908dcbd95aa297c2a23e585f6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:06:38 -0600

    Update ytdl.js

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { rateLimit } = require('express-rate-limit');
-const apicache = require("apicache");
 const YT = require('./func/YT_mp3_mp4');
 
-const app = express();
-
-const apiRequestLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 40,
-    handler: function (req, res) {
-        return res.status(429).json({ error: 'Rate limit exceeded' });
-    }
-});
-
-const cache = apicache.middleware;
-
-router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
+router.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     const match_url = req.query.url;
     try {
