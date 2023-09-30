fbf3ffffd3f74576815d84853674c869b3c99717
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:03:41 -0600

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { rateLimit } = require('express-rate-limit');
-const apicache = require("apicache");
-const cache = apicache.middleware
 
-const apiRequestLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 40,
-    handler: function (req, res) {
-        return res.status(429).json(
-          dummydata()
-        )
-    }
-})
-
-router.get('/', cache('1 hour'), apiRequestLimiter, function(req, res) {
+router.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.json('Api Rest - By BrunoSobrino - En Desarollo');
 });
