5cfdf92244f84014fc2e463fdca35cb3875b83f5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:04:48 -0600

    Update spotifysearch.js

--- routes/spotifysearch.js
+++ routes/spotifysearch.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { rateLimit } = require('express-rate-limit');
-const apicache = require('apicache');
 const { getMusicBuffer } = require('./func/spotifysearch'); 
 
-const app = express();
-
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 40,
-  handler: function(req, res) {
-    return res.status(429).json({ error: 'Rate limit exceeded' });
-  },
-});
-
-const cache = apicache.middleware;
-
-router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
+router.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'audio/mpeg');
-
   const songName = req.query.songName; 
-
   try {
     const songBuffer = await getMusicBuffer(songName); 
     res.end(songBuffer);
