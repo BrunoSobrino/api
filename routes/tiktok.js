4cb4c11c769ea99ceec5ba0b1cfe92b78e68866d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:05:12 -0600

    Update tiktok.js

--- routes/tiktok.js
+++ routes/tiktok.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { rateLimit } = require('express-rate-limit');
-const apicache = require('apicache');
 const { getTikTokBuffer } = require('./func/tiktokdl');
 
-const app = express();
-
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 40,
-  handler: function(req, res) {
-    return res.status(429).json({ error: 'Rate limit exceeded' });
-  },
-});
-
-const cache = apicache.middleware;
-
-router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
+router.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'video/mp4');
-
   const match_url = req.query.url;
-
   try {
     const videoBuffer = await getTikTokBuffer(match_url);
     res.send(videoBuffer);
