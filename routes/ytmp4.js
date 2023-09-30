de6046a421134657d8bfa4f85cb67a47715ceac1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 23:30:45 -0600

    Update ytmp4.js

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -6,9 +6,16 @@ const YT = require('./func/YT_mp3_mp4');
 router.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'video/mp4');
   const match_url = req.query.url;
+  let videoPath;
   try {
     const videoData = await YT.mp4(match_url);
-    const fileStream = fs.createReadStream(videoData.path);
+    videoPath = videoData.path;
+    const fileStream = fs.createReadStream(videoPath);
+    fileStream.on('close', () => {
+      if (fs.existsSync(videoPath)) {
+        fs.unlinkSync(videoPath);
+      }
+    });
     fileStream.pipe(res);
   } catch (error) {
     if (!error.response) {
