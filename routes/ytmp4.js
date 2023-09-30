b34384063262f703af16a97522a2500061678ec3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:08:24 -0600

    Update ytmp4.js

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { rateLimit } = require('express-rate-limit');
-const apicache = require('apicache');
 const fs = require('fs');
 const YT = require('./func/YT_mp3_mp4');
 
-const app = express();
-
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 40,
-  handler: function(req, res) {
-    return res.status(429).json({ error: 'Rate limit exceeded' });
-  },
-});
-
-const cache = apicache.middleware;
-
 router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
-  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('X-Frame-Options', 'DENY');
-  res.header('X-XSS-Protection', '1; mode=block');
-  res.header('X-Content-Type-Options', 'nosniff');
-  res.header('Strict-Transport-Security', 'max-age=63072000');
   res.setHeader('Content-Type', 'video/mp4');
-
   const match_url = req.query.url;
-
   try {
     const videoData = await YT.mp4(match_url);
     const fileStream = fs.createReadStream(videoData.path);
