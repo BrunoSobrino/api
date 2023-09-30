8a2406caa114eab535f257a8b4f29bc1dae0cb06
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-29 22:07:05 -0600

    Update ytmp3.js

--- routes/ytmp3.js
+++ routes/ytmp3.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { rateLimit } = require('express-rate-limit');
-const apicache = require('apicache');
 const YT = require('./func/YT_mp3_mp4');
 
-const app = express();
-
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 40,
-  handler: function(req, res) {
-    return res.status(429).json({ error: 'Rate limit exceeded' });
-  },
-});
-
-const cache = apicache.middleware;
-
-router.get('/', cache('2 minutes'), apiRequestLimiter, async (req, res) => {
+router.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'audio/mpeg');
-
   const match_url = req.query.url;
-
   try {
     const audioBuffer = await YT.mp3(match_url);
     res.end(audioBuffer);
