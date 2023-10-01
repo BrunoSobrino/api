ce3fc6f1a6ebd9ecf9fa0a82a168e029089cbee2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-01 12:17:42 -0600

    Update chatgpt.js

--- routes/chatgpt.js
+++ routes/chatgpt.js
@@ -3,21 +3,13 @@ const router = express.Router();
 const chatgpt = require('./func/chatgpt'); 
 
 router.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
   const inputText = req.query.text;
   const lenguaje = req.query.lenguaje || 'es'; 
-  try {
-    if (!inputText) {
-      throw new Error('Texto de entrada no especificado');
-    }
+   try {
     const results = await chatgpt(inputText, lenguaje);
     const formattedResults = JSON.stringify(results, null, 2);
-    res.setHeader('Content-Type', 'application/json');
-    const correctEscapeSequences = async function (text) {
-    const correctedText = text.replace(/\\U/g, "\\u").replace(/\\f/g, "\\u00");
-      return correctedText;
-    }
-    const finr = await correctEscapeSequences(formattedResults)
-    res.send(finr);
+    res.send(formattedResults);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error en la solicitud de chatgpt' });
