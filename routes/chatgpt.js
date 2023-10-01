954b550d34d338183627edfc78699d923620bf0f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-30 23:05:54 -0600

    Update chatgpt.js

--- routes/chatgpt.js
+++ routes/chatgpt.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { chatgpt } = require('./func/chatgpt'); 
+const chatgpt = require('./func/chatgpt'); 
 
 router.get('/', async (req, res) => {
   const inputText = req.query.text;
