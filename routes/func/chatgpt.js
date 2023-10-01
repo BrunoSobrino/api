56daa1afadc7b594b73d70ecddd14062479db292
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-01 12:18:23 -0600

    Update chatgpt.js

--- routes/func/chatgpt.js
+++ routes/func/chatgpt.js
@@ -2,103 +2,123 @@ const fetch = require('node-fetch');
 const translate = require('@vitalets/google-translate-api');
 
 async function chatgpt(text, lenguaje = 'es') {
+  if (!text) {
+    return {
+      status: false,
+      message: "No has ingresado un texto.",
+      ejemplo: "https://api.boxmine.xyz/chatgpt?text=hola&lenguaje=es"
+    };
+  }    
   const result = {
     status: true,
     resultado: "",
   };
-
-  try {
-    const fgapi1 = await fetch(`https://api-fgmods.ddns.net/api/info/openai2?text=${text}&apikey=XlwAnX8d`);
-    const fgjson1 = await fgapi1.json();
-    if (fgjson1?.result != 'error' && fgjson1?.result != '' && fgjson1?.result != undefined && fgjson1?.result) {
-      const translatedResult = await translate(fgjson1.result, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const vihangayt1 = await fetch(`https://vihangayt.me/tools/chatgpt?q=${text}`);
-    const vihangaytjson1 = await vihangayt1.json();
-    if (vihangaytjson1?.data != 'error' && vihangaytjson1?.data != '' && vihangaytjson1?.data != undefined && vihangaytjson1?.data) {
-      let parsedData1 = ''; 
-      try {     
-        parsedData1 = unescape(vihangaytjson1.data);
-      } catch {
-        parsedData1 = vihangaytjson1.data;    
-      }  
-      const translatedResult = await translate(parsedData1, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const vihangayt2 = await fetch(`https://vihangayt.me/tools/chatgpt2?q=${text}`);
-    const vihangaytjson2 = await vihangayt2.json();
-    if (vihangaytjson2?.data != 'error' && vihangaytjson2?.data != '' && vihangaytjson2?.data != undefined && vihangaytjson2?.data) {
-      let parsedData2 = ''; 
-      try {     
-        parsedData2 = unescape(vihangaytjson2.data);
-      } catch {
-        parsedData2 = vihangaytjson2.data;    
-      }  
-      const translatedResult = await translate(parsedData2, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const vihangayt3 = await fetch(`https://vihangayt.me/tools/chatgpt3?q=${text}`);
-    const vihangaytjson3 = await vihangayt3.json();
-    if (vihangaytjson3?.data != 'error' && vihangaytjson3?.data != '' && vihangaytjson3?.data != undefined && vihangaytjson3?.data) {
-      let parsedData3 = ''; 
-      try {     
-        parsedData3 = unescape(vihangaytjson3.data);
-      } catch {
-        parsedData3 = vihangaytjson3.data;    
-      }  
-      const translatedResult = await translate(parsedData3, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const tioress22 = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=GataDios&text=${text}&user=apirest`);
-    const hasill22 = await tioress22.json();
-    if (hasill22?.result != 'error' && hasill22?.result != '' && hasill22?.result != undefined && hasill22?.result) {
-      const translatedResult = await translate(hasill22.result, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const searchString2 = ' Indonesia ';
-    const replacementString2 = ' español ';
-    const rres = await fetch(`https://api.ibeng.tech/api/others/chatgpt?q=${text}&apikey=eMlBNRzUXv`);
-    const jjson = await rres.json();
-    if (jjson?.data != 'error' && jjson?.data != '' && jjson?.data != undefined && jjson?.data) {
-      const hahaha = await translate(jjson.data, { to: lenguaje, autoCorrect: true });
-      const sextS = hahaha.text;
-      const replacedText = sextS.replace(searchString2, replacementString2).trim();
-      result.resultado = replacedText;
-      return result;
-    }
-
-    const akuariapi2 = await fetch(`https://api.akuari.my.id/ai/gpt?chat=${text}`);
-    const akuariapijson2 = await akuariapi2.json();
-    if (akuariapijson2?.respon != 'error' && akuariapijson2?.respon != '' && akuariapijson2?.respon != undefined && akuariapijson2?.respon) {
-      const translatedResult = await translate(akuariapijson2.respon, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-
-    const akuariapi1 = await fetch(`https://api.azz.biz.id/api/bard?q=${text}&key=global`);
-    const akuariapijson1 = await akuariapi1.json();
-    if (akuariapijson1?.respon != 'error' && akuariapijson1?.respon != '' && akuariapijson1?.respon != undefined && akuariapijson1?.respon) {
-      const translatedResult = await translate(akuariapijson1.respon, { to: lenguaje, autoCorrect: true });
-      result.resultado = translatedResult.text.trim();
-      return result;
-    }
-  } catch (e) {
-    console.log(e);
-    result.status = false;
-    result.resultado = "Error en todas las APIs";
-    return result;
+  const apiEndpoints = [
+    {
+      url: `https://api-fgmods.ddns.net/api/info/openai2?text=${text}&apikey=XlwAnX8d`,
+      processResponse: async (data) => {
+        if (data?.result != 'error' && data?.result != '' && data?.result != undefined && data?.result) {
+          const translatedResult = await translate(data.result, { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://vihangayt.me/tools/chatgpt?q=${text}`,
+      processResponse: async (data) => {
+        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
+          let parsedData = '';
+          try {
+            parsedData = unescape(data.data);
+          } catch {
+            parsedData = data.data;
+          }
+          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://vihangayt.me/tools/chatgpt2?q=${text}`,
+      processResponse: async (data) => {
+        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
+          let parsedData = '';
+          try {
+            parsedData = unescape(data.data);
+          } catch {
+            parsedData = data.data;
+          }
+          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://vihangayt.me/tools/chatgpt3?q=${text}`,
+      processResponse: async (data) => {
+        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
+          let parsedData = '';
+          try {
+            parsedData = unescape(data.data);
+          } catch {
+            parsedData = data.data;
+          }
+          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://api.lolhuman.xyz/api/openai?apikey=GataDios&text=${text}&user=apirest`,
+      processResponse: async (data) => {
+        if (data?.result != 'error' && data?.result != '' && data?.result != undefined && data?.result) {
+          const translatedResult = await translate(data.result, { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://api.ibeng.tech/api/others/chatgpt?q=${text}&apikey=eMlBNRzUXv`,
+      processResponse: async (data) => {
+        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
+          const hahaha = await translate(data.data, { to: lenguaje, autoCorrect: true });
+          const sextS = hahaha.text;
+          const replacedText = sextS.replace(' Indonesia ', ' español ').trim();
+          result.resultado = replacedText;
+        }
+      },
+    },
+    {
+      url: `https://api.akuari.my.id/ai/gpt?chat=${text}`,
+      processResponse: async (data) => {
+        if (data?.respon != 'error' && data?.respon != '' && data?.respon != undefined && data?.respon) {
+          const translatedResult = await translate(data.respon, { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+    {
+      url: `https://api.azz.biz.id/api/bard?q=${text}&key=global`,
+      processResponse: async (data) => {
+        if (data?.respon != 'error' && data?.respon != '' && data?.respon != undefined && data?.respon) {
+          const translatedResult = await translate(data.respon, { to: lenguaje, autoCorrect: true });
+          result.resultado = translatedResult.text.trim();
+        }
+      },
+    },
+  ];
+  for (const apiEndpoint of apiEndpoints) {  
+      const response = await fetch(apiEndpoint.url);
+      const responseData = await response.json();
+      await apiEndpoint.processResponse(responseData);
+      if (result.resultado) {
+        result.resultado = result.resultado;
+        return result;
+      }
   }
+  result.status = false;
+  result.resultado = "Error en todas las APIs";
+  return result;
 }
 
 module.exports = chatgpt;
