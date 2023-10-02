29cf0f94a16f4e4fd68c2579c4fb3ec8d1038c11
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-01 18:27:44 -0600

    Update tiktok.js

--- routes/tiktok.js
+++ routes/tiktok.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const { getTikTokBuffer } = require('./func/tiktokdl');
 
 router.get('/', async (req, res) => {
-  res.setHeader('Content-Type', 'video/mp4');
-  const match_url = req.query.url;
   try {
-    const videoBuffer = await getTikTokBuffer(match_url);
-    res.send(videoBuffer);
-  } catch (error) {
-    if (!error.response) {
-      res.status(500).send('An error occurred');
-    } else {
-      res.status(500).send('An error occurred');
+    const link = req.query.url; 
+    const videoData = await getTikTokBuffer(link); 
+    const videoBuffer = Buffer.from(videoData.buffer); 
+    let fileName = `video_${Date.now()}.mp4`;
+    let fileIndex = 1;
+    while (fs.existsSync(`./tmp/${fileName}`)) {
+      const extension = path.extname(fileName);
+      const baseName = path.basename(fileName, extension);
+      fileName = `${baseName}_${fileIndex}${extension}`;
+      fileIndex++;
     }
+    fs.writeFileSync(`./tmp/${fileName}`, videoBuffer);
+    res.sendFile(fileName, { root: './tmp' }, (err) => {
+      if (err) {
+        console.error('Error al enviar el archivo:', err);
+        res.status(500).json({ error: 'Ocurrió un error al enviar el archivo' });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+    console.error(error);
   }
 });
 
