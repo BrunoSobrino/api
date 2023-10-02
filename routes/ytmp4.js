30ddf9e828da6afbe5cc282aec450190565075aa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-01 18:19:19 -0600

    Update ytmp4.js

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const YT = require('./func/YT_mp3_mp4');
+const path = require('path');
+const YT = require('./func/YT_mp3_mp4'); 
 
 router.get('/', async (req, res) => {
-  res.setHeader('Content-Type', 'video/mp4');
-  const match_url = req.query.url;
-  let videoPath;
   try {
-    const videoData = await YT.mp4(match_url);
-    videoPath = videoData.path;
-    const fileStream = fs.createReadStream(videoPath);
-    fileStream.on('close', () => {
-      if (fs.existsSync(videoPath)) {
-        fs.unlinkSync(videoPath);
+    const link = req.query.url; 
+    const videoData = await YT.mp4(link); 
+    const videoPath = videoData.path;
+    let fileName = path.basename(videoPath);
+    let fileIndex = 1;
+    while (fs.existsSync(`./tmp/${fileName}`)) {
+      const extension = path.extname(fileName);
+      const baseName = path.basename(fileName, extension);
+      fileName = `${baseName}_${fileIndex}${extension}`;
+      fileIndex++;
+    }
+    fs.renameSync(videoPath, `./tmp/${fileName}`);
+    res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'video/mp4' } }, (err) => {
+      if (err) {
+        console.error('Error al enviar el archivo:', err);
+        res.status(500).json({ error: 'Ocurrió un error al enviar el archivo' });
       }
     });
-    fileStream.pipe(res);
   } catch (error) {
-    if (!error.response) {
-      res.status(500).send('An error occurred');
-    } else {
-      res.status(500).send('An error occurred');
-    }
+    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+    console.error(error);
   }
 });
 
