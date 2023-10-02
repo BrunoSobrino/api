cb7a594eb492115c318a4078de45087839d457bc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-01 18:25:21 -0600

    Update ytmp3_2.js

--- routes/ytmp3_2.js
+++ routes/ytmp3_2.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const YT = require('./func/YT_mp3_mp4');
 
 router.get('/', async (req, res) => {
-  res.setHeader('Content-Type', 'audio/mpeg');
-  const match_url = req.query.url;
   try {
+    const match_url = req.query.url;
     const audioBuffer = await YT.mp3_2(match_url);
-    res.end(audioBuffer);
-  } catch (error) {
-    if (!error.response) {
-      res.status(500).send('An error occurred');
-    } else {
-      res.status(500).send('An error occurred');
+    let fileName = `audio_${Date.now()}.mp3`;
+    let fileIndex = 1;
+    while (fs.existsSync(`./tmp/${fileName}`)) {
+      fileName = `audio_${Date.now()}_${fileIndex}.mp3`;
+      fileIndex++;
     }
+    fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
+    res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'audio/mpeg' } }, (err) => {
+      if (err) {
+        console.error('Error al enviar el archivo:', err);
+        res.status(500).json({ error: 'Ocurrió un error al enviar el archivo' });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+    console.error(error);
   }
 });
 
