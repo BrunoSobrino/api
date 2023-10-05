c99c1df8573c91c82ae7911bc99db93cb64d6926
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-05 17:31:48 -0600

    Update igdl.js

--- routes/func/igdl.js
+++ routes/func/igdl.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const { fromBuffer  } = require('file-type');
 const instagramDl = require("@sasmeee/igdl");
+const instagramGetUrl = require('instagram-url-direct');
+const { instagram } = require('@xct007/frieren-scraper');
+const { instagramdl } = require('@bochilteam/scraper');
 
 async function downloadInstagramContent(url) {
   try {
@@ -40,6 +43,119 @@ async function downloadInstagramContent(url) {
   }
 }
 
+async function igdl2(url) {
+  try {
+    const datTa = await instagram.v1(url);
+    if (!datTa || datTa.length === 0) {
+      throw new Error('No se encontraron datos de Instagram para la URL proporcionada');
+    }
+    const formattedData = [];
+    for (const urRRl of datTa) {
+      if (urRRl && urRRl.url) {
+        const { buffer, detectedType } = await getBuffer(urRRl.url);
+        if (detectedType && (detectedType.mime.startsWith('image/') || detectedType.mime.startsWith('video/'))) {
+          formattedData.push({
+            type: detectedType.mime.startsWith('image/') ? 'imagen' : 'video',
+            url_download: urRRl.url
+          });
+        }
+      }
+    }
+    if (formattedData.length === 0) {
+      throw new Error('No se encontraron imágenes ni videos en la URL de Instagram proporcionada');
+    }
+    return {
+      success: true,
+      data: formattedData
+    };
+  } catch {
+  try {
+    const resultss = await instagramGetUrl(url);
+    if (!resultss || resultss.length === 0) {
+      throw new Error('No se encontraron datos de Instagram para la URL proporcionada');
+    }
+    const formattedData = [];
+    for (const dataresult of resultss) {
+      if (dataresult && dataresult.url_list) {
+        const { buffer, detectedType } = await getBuffer(dataresult.url_list);
+        if (detectedType && (detectedType.mime.startsWith('image/') || detectedType.mime.startsWith('video/'))) {
+          formattedData.push({
+            type: detectedType.mime.startsWith('image/') ? 'imagen' : 'video',
+            url_download: dataresult.url_list
+          });
+        }
+      }
+    }
+    if (formattedData.length === 0) {
+      throw new Error('No se encontraron imágenes ni videos en la URL de Instagram proporcionada');
+    }
+    return {
+      success: true,
+      data: formattedData
+    };        
+} catch {
+try {    
+    const resultssss = await instagramdl(url);
+    if (!resultssss || resultssss.length === 0) {
+      throw new Error('No se encontraron datos de Instagram para la URL proporcionada');
+    }
+    const formattedData = [];
+    for (const tiosex of resultssss) {
+      if (tiosex) {
+        const { buffer, detectedType } = await getBuffer(tiosex);
+        if (detectedType && (detectedType.mime.startsWith('image/') || detectedType.mime.startsWith('video/'))) {
+          formattedData.push({
+            type: detectedType.mime.startsWith('image/') ? 'imagen' : 'video',
+            url_download: tiosex
+          });
+        }
+      }
+    }
+    if (formattedData.length === 0) {
+      throw new Error('No se encontraron imágenes ni videos en la URL de Instagram proporcionada');
+    }
+    return {
+      success: true,
+      data: formattedData
+    };   
+} catch {
+try {
+     const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=GataDios&url=${url}`);
+     const json = await human.json();
+     const videoig = json.result;
+    if (!videoig || videoig.length === 0) {
+      throw new Error('No se encontraron datos de Instagram para la URL proporcionada');
+    }
+    const formattedData = [];
+    for (const alaprimaselearrima of videoig) {
+      if (alaprimaselearrima) {
+        const { buffer, detectedType } = await getBuffer(alaprimaselearrima);
+        if (detectedType && (detectedType.mime.startsWith('image/') || detectedType.mime.startsWith('video/'))) {
+          formattedData.push({
+            type: detectedType.mime.startsWith('image/') ? 'imagen' : 'video',
+            url_download: alaprimaselearrima
+          });
+        }
+      }
+    }
+    if (formattedData.length === 0) {
+      throw new Error('No se encontraron imágenes ni videos en la URL de Instagram proporcionada');
+    }
+    return {
+      success: true,
+      data: formattedData
+    };       
+} catch (error) {
+    return {
+      success: false,
+      error: error.message
+     };
+    }
+   }   
+  }
+ }
+}
+
 async function getBuffer(url, options) {
   try {
     options = options || {};
@@ -59,4 +175,4 @@ async function getBuffer(url, options) {
   }
 }
 
-module.exports = { downloadInstagramContent };
+module.exports = { downloadInstagramContent, igdl2 };
