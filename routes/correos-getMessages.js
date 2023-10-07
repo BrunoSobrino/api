a1116b56c3f4c8b19bc8b5fbb60730df41f4930f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-07 15:37:13 -0600

    Update correos-getMessages.js

--- routes/correos-getMessages.js
+++ routes/correos-getMessages.js
@@ -3,13 +3,22 @@ const router = express.Router();
 const { obtenerCorreos } = require('./func/tempmail'); 
 
 router.get('/', async (req, res) => {
-  const correoCompleto = req.query.correo;
+  const correoCompleto = req.query.mail;
   try {
+    if (!correoCompleto) {
+      const errorMessage = 'Debes proporcionar un correo';
+      const formattedError = JSON.stringify({
+        status: false,
+        message: errorMessage
+      }, null, 2);
+      return res.status(200).json(JSON.parse(formattedError));
+    }
     const correos = await obtenerCorreos(correoCompleto);
-      res.status(200).json({
-        status: true,
-        correos: correos.correos
-      })
+    const formattedResponse = JSON.stringify({
+      status: true,
+      correos: correos.correos
+    }, null, 2);
+    res.status(200).json(JSON.parse(formattedResponse));
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error al obtener los correos' });
