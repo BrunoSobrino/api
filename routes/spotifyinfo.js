593b54cacda7a45c5284f0f9a9839cf0076ce95f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-07 16:16:40 -0600

    Create spotifyinfo.js

---
+++ routes/spotifyinfo.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { spotifySearch1 } = require('./func/spotify');
+
+router.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');  
+  const textoo = req.query.text;
+  try {
+    if (!textoo) {
+      const formattedError = {status: false, message: 'Debes proporcionar un texto o link de spotify.'};
+      const formattedResults = JSON.stringify(formattedError, null, 2);
+      return res.send(formattedResults);
+    }
+    const spty = await spotifySearch1(correoCompleto);
+    const formattedResponse = {status: true, spty};
+    const formattedResults2 = JSON.stringify(formattedResponse, null, 2);
+    res.send(formattedResults2);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener los resultados.' });
+  }
+});
+
+module.exports = router;
