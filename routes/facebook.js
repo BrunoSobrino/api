5ef77de907c2a251b178613fba2fa784feef8b3e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-08 15:55:17 -0600

    Create facebook.js

---
+++ routes/facebook.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { facebook } = require('./func/facebook'); 
+
+router.get('/', async (req, res) => {
+  const url = req.query.url;
+  try {
+    const results = await facebook(url);
+      const formattedResults = JSON.stringify(results, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error en la descarga de contenido de Facebook' });
+  }
+});
+
+module.exports = router;
