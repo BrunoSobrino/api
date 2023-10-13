4ee1c41a389d7a41f8cf79edb909f82cfe59cd48
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-13 17:35:05 -0600

    Create packgirl.js

---
+++ routes/packgirl.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/packgirl.json');
+    const data = response.data;
+    const randomIndex = Math.floor(data.length * Math.random());
+    const imageUrl = data[randomIndex];
+    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(imageBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+module.exports = router;
