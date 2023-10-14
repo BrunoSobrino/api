5fa806788ac2b98b4eff673f7a8f23bd2b19b2d7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-13 19:48:21 -0600

    Update videoxxxlesbi.js

--- routes/videoxxxlesbi.js
+++ routes/videoxxxlesbi.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+
+const stat = promisify(fs.stat);
 
 router.get('/', async (req, res) => {
   try {
@@ -10,32 +13,42 @@ router.get('/', async (req, res) => {
     const data = response.data;
     const randomIndex = Math.floor(data.length * Math.random());
     const videoUrl = data[randomIndex];
-    const tmpDirectory = path.join(__dirname, '..', 'tmp'); // Ruta absoluta
-    fs.mkdirSync(tmpDirectory, { recursive: true }); // Asegura que la carpeta tmp exista
+    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    const videoBuffer = Buffer.from(videoResponse.data, 'binary');
 
-    let videoFileName = `video_${Date.now()}.mp4`;
-    let fileIndex = 1;
+    // Establece el tipo de contenido de la respuesta como 'video/mp4' (cámbialo según el formato del video).
+    res.setHeader('Content-Type', 'video/mp4');
 
-    // Genera un nombre de archivo único con sufijo numérico
-    while (fs.existsSync(path.join(tmpDirectory, videoFileName))) {
-      videoFileName = `video_${Date.now()}_${fileIndex}.mp4`;
-      fileIndex++;
-    }
+    // Genera un nombre de archivo aleatorio y verifica si ya existe en el directorio `tmp`.
+    let videoFileName = generateRandomFileName('tmp', 'video', '.mp4');
 
-    const videoFilePath = path.join(tmpDirectory, videoFileName);
+    // Escribe el videoBuffer en un archivo temporal en el servidor.
+    const videoFilePath = path.join(__dirname, videoFileName);
+    fs.writeFileSync(videoFilePath, videoBuffer);
 
-    const videoResponse = await axios.get(videoUrl, { responseType: 'stream' });
-    const videoStream = videoResponse.data;
-    const writeStream = fs.createWriteStream(videoFilePath);
-    videoStream.pipe(writeStream);
-
-    writeStream.on('finish', () => {
-      res.sendFile(videoFileName, { root: tmpDirectory, headers: { 'Content-Type': 'video/mp4' } });
-    });
+    // Usa res.sendFile() para enviar el archivo de video al cliente.
+    res.sendFile(videoFileName, { root: __dirname });
   } catch (error) {
-    console.error('Ocurrió un error al procesar la solicitud:', error);
-    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+    console.error(error);
+
+    // En caso de error, envía una respuesta de error al cliente con un mensaje.
+    res.status(500).send('An error occurred');
   }
 });
+
+// Función para generar un nombre de archivo aleatorio que no existe en el directorio.
+function generateRandomFileName(directory, baseName, extension) {
+  let counter = 0;
+  let fileName = `${baseName}${counter}${extension}`;
+  const filePath = path.join(directory, fileName);
+
+  // Verifica si el archivo con el nombre generado ya existe en el directorio.
+  while (fs.existsSync(filePath)) {
+    counter++;
+    fileName = `${baseName}${counter}${extension}`;
+  }
+
+  return fileName;
+}
 
 module.exports = router;
