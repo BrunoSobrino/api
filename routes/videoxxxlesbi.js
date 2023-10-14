f0998ec841bf40f5c31bb7c6bec4de2d0240579b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 09:21:48 -0600

    Update videoxxxlesbi.js

--- routes/videoxxxlesbi.js
+++ routes/videoxxxlesbi.js
@@ -4,6 +4,19 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
+function RandomAgresivo(min, max) {
+  if (typeof RandomAgresivo.seen === 'undefined') {
+    RandomAgresivo.seen = [];
+  }
+  if (RandomAgresivo.seen.length === 0) {
+    RandomAgresivo.seen = Array.from({ length: max - min + 1 }, (_, index) => index + min);
+  }
+  const randomIndex = Math.floor(Math.random() * RandomAgresivo.seen.length);
+  const randomNumber = RandomAgresivo.seen[randomIndex];
+  RandomAgresivo.seen.splice(randomIndex, 1);
+  return randomNumber;
+}
+
 router.get('/', async (req, res) => {
   try {
     res.setHeader('Content-Type', 'video/mp4');
@@ -12,11 +25,10 @@ router.get('/', async (req, res) => {
     res.setHeader('Expires', '0');
     const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videolesbixxx.json');
     const data = response.data;
-    const randomIndex = Math.floor(data.length * Math.random());
+    const randomIndex = RandomAgresivo(0, data.length - 1);
     const videoUrl = data[randomIndex];
     const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
     const videoBuffer = Buffer.from(videoResponse.data, 'base64');
-    //const videoBase64 = videoBuffer.toString('base64');
     res.end(videoBuffer);
   } catch (error) {
     console.error(error);
