19a9b94229fd539b1e7dd24ff6634a026383317f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 11:15:14 -0600

    Update functions.js

--- routes/func/functions.js
+++ routes/func/functions.js
@@ -81,10 +81,67 @@ const getBuffer2 = async (url, options) => {
   };
 };
 
+function xnxxdl(URL) {
+  return new Promise((resolve, reject) => {
+    fetch(`${URL}`, {method: 'get'}).then((res) => res.text()).then((res) => {
+      const $ = cheerio.load(res, {xmlMode: false});
+      const title = $('meta[property="og:title"]').attr('content');
+      const duration = $('meta[property="og:duration"]').attr('content');
+      const image = $('meta[property="og:image"]').attr('content');
+      const videoType = $('meta[property="og:video:type"]').attr('content');
+      const videoWidth = $('meta[property="og:video:width"]').attr('content');
+      const videoHeight = $('meta[property="og:video:height"]').attr('content');
+      const info = $('span.metadata').text();
+      const videoScript = $('#video-player-bg > script:nth-child(6)').html();
+      const files = {
+        low: (videoScript.match('html5player.setVideoUrlLow\\(\'(.*?)\'\\);') || [])[1],
+        high: videoScript.match('html5player.setVideoUrlHigh\\(\'(.*?)\'\\);' || [])[1],
+        HLS: videoScript.match('html5player.setVideoHLS\\(\'(.*?)\'\\);' || [])[1],
+        thumb: videoScript.match('html5player.setThumbUrl\\(\'(.*?)\'\\);' || [])[1],
+        thumb69: videoScript.match('html5player.setThumbUrl169\\(\'(.*?)\'\\);' || [])[1],
+        thumbSlide: videoScript.match('html5player.setThumbSlide\\(\'(.*?)\'\\);' || [])[1],
+        thumbSlideBig: videoScript.match('html5player.setThumbSlideBig\\(\'(.*?)\'\\);' || [])[1]};
+      resolve({status: true, result: {title, URL, duration, image, videoType, videoWidth, videoHeight, info, files}});
+    }).catch((err) => reject({status: false, result: err}));
+  });
+}
+
+function xnxxsearch(query) {
+  return new Promise((resolve, reject) => {
+    const baseurl = 'https://www.xnxx.com';
+    fetch(`${baseurl}/search/${query}/${Math.floor(Math.random() * 3) + 1}`, {method: 'get'}).then((res) => res.text()).then((res) => {
+      const $ = cheerio.load(res, {xmlMode: false});
+      const title = [];
+      const url = [];
+      const desc = [];
+      const results = [];
+      $('div.mozaique').each(function(a, b) {
+        $(b).find('div.thumb').each(function(c, d) {
+          url.push(baseurl + $(d).find('a').attr('href').replace('/THUMBNUM/', '/'));
+        });
+      });
+      $('div.mozaique').each(function(a, b) {
+        $(b).find('div.thumb-under').each(function(c, d) {
+          desc.push($(d).find('p.metadata').text());
+          $(d).find('a').each(function(e, f) {
+            title.push($(f).attr('title'));
+          });
+        });
+      });
+      for (let i = 0; i < title.length; i++) {
+        results.push({title: title[i], info: desc[i], link: url[i]});
+      }
+      resolve({status: true, result: results});
+    }).catch((err) => reject({status: false, result: err}));
+  });
+}
+
 
 module.exports = {
     getBuffer,
     getBuffer2,
     RandomAgresivo,
-    getFileName
+    getFileName,
+    xnxxsearch,
+    xnxxdl
 };
