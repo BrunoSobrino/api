dfc66237906227f2abb386fa96ba35db77305cea
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-13 18:32:05 -0600

    Create videoxxx.js

---
+++ routes/videoxxx.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
+    const data = response.data;
+    const randomIndex = Math.floor(data.length * Math.random());
+    const videoUrl = data[randomIndex];
+    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    const videoBuffer = Buffer.from(videoResponse.data, 'binary');
+    res.setHeader('Content-Type', 'video/mp4'); // Cambiar 'video/mp4' al tipo de contenido adecuado
+    res.send(videoBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+module.exports = router;
