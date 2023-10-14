be21f08a602efbba3691f375a0381308bcc722e8
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-13 19:28:09 -0600

    Update videoxxxlesbi.js

--- routes/videoxxxlesbi.js
+++ routes/videoxxxlesbi.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', async (req, res) => {
   try {
@@ -8,14 +10,30 @@ router.get('/', async (req, res) => {
     const data = response.data;
     const randomIndex = Math.floor(data.length * Math.random());
     const videoUrl = data[randomIndex];
-    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
-    const videoBuffer = Buffer.from(videoResponse.data, 'binary');
-    res.setHeader('Content-Type', 'video/mp4'); // Cambiar 'video/mp4' al tipo de contenido adecuado
-    res.send(videoBuffer);
+    const tmpDirectory = path.join('../tmp');
+    const videoFileName = generateUniqueFileName('video.mp4', tmpDirectory);
+    const videoFilePath = path.join(__dirname, tmpDirectory, videoFileName);
+    const videoResponse = await axios.get(videoUrl, { responseType: 'stream' });
+    const videoStream = videoResponse.data;
+    const writeStream = fs.createWriteStream(videoFilePath);
+    videoStream.pipe(writeStream);
+    writeStream.on('finish', () => {
+      res.sendFile(videoFilePath);
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred');
   }
 });
+
+function generateUniqueFileName(baseName, directory) {
+  let timestamp = new Date().getTime();
+  let uniqueName = `${timestamp}_${baseName}`;
+  while (fs.existsSync(path.join(directory, uniqueName))) {
+    timestamp++;
+    uniqueName = `${timestamp}_${baseName}`;
+  }
+  return uniqueName;
+}
 
 module.exports = router;
