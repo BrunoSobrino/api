1389fa7d28388de6c2e46bbd27eee970ec1361c5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 13:18:13 -0600

    Create xnxxdl.js

---
+++ routes/xnxxdl.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { xnxxdl } = require('./func/functions');
+
+router.get('/xnxxdl', async (req, res) => {
+  const videoUrl = req.query.url;
+  try {
+    if (!videoUrl) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar una URL de video'
+      };
+      res.status(400).json(errorResponse);
+    } else {
+      const result = await xnxxdl(videoUrl);
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, message: 'Error al obtener los datos de XNXX' });
+  }
+});
+
+module.exports = router;
