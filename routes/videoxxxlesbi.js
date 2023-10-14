4df9d59bf6ba00007e2dc18fffb85139422bc45d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 08:50:34 -0600

    Update videoxxxlesbi.js

--- routes/videoxxxlesbi.js
+++ routes/videoxxxlesbi.js
@@ -12,29 +12,8 @@ router.get('/', async (req, res) => {
     const videoUrl = data[randomIndex];
     const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
     const videoBuffer = Buffer.from(videoResponse.data, 'binary');
-    /*const tmpDirectory = path.join(__dirname, '..', 'tmp');
-    if (!fs.existsSync(tmpDirectory)) {
-      fs.mkdirSync(tmpDirectory);
-    }
-    let counter = 0;
-    let videoFileName = 'video.mp4';
-    let videoFilePath = path.join(tmpDirectory, videoFileName);
-    while (fs.existsSync(videoFilePath)) {
-      counter++;
-      videoFileName = `video_${counter}.mp4`;
-      videoFilePath = path.join(tmpDirectory, videoFileName);
-    }
-    fs.writeFileSync(videoFilePath, videoBuffer);
-    res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Cache-Control', 'no-store, max-age=0');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    if (fs.existsSync(videoFilePath)) {*/
-      res.end(videoBuffer) //videoFileName, { root: tmpDirectory });
-    /*} else {
-      console.error('El archivo no existe en la ubicación especificada.');
-      res.status(404).send('File not found');
-    }*/
+    const videoBase64 = videoBuffer.toString('base64');
+    res.send(videoBase64);
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred');
