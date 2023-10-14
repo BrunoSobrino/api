95ebad4e292df112aeb7de2a6366d30560dba806
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 07:16:53 -0600

    Update videoxxxlesbi.js

--- routes/videoxxxlesbi.js
+++ routes/videoxxxlesbi.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videolesbixxx.json');
     const data = response.data;
     const randomIndex = Math.floor(data.length * Math.random());
-    const videoUrl = data[randomIndex];
+    const videoUrl = data[randomIndex] + `?nocache=${Date.now()}`;
     const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
     const videoBuffer = Buffer.from(videoResponse.data, 'binary');
 
