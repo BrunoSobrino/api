8a478e2294268b482ac61c1b733d7aa22123694b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 10:54:10 -0600

    Update videoxxx.js

--- routes/videoxxx.js
+++ routes/videoxxx.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const { RandomAgresivo, getFileName } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
+    const data = response.data;
+    const randomIndex = RandomAgresivo(0, data.length - 1);
+    const videoUrl = data[randomIndex];
+    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    const videoBuffer = Buffer.from(videoResponse.data, 'base64');
+    res.end(videoBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+module.exports = router;
+
+
+/*const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
 const { RandomAgresivo, getFileName } = require('./func/functions');
@@ -30,32 +59,5 @@ router.get('/', async (req, res) => {
   }
 });
 
-module.exports = router;
-
-/*const express = require('express');
-const router = express.Router();
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const { RandomAgresivo, getFileName } = require('./func/functions');
-
-router.get('/', async (req, res) => {
-  try {
-    res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Cache-Control', 'no-store, max-age=0');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
-    const data = response.data;
-    const randomIndex = RandomAgresivo(0, data.length - 1);
-    const videoUrl = data[randomIndex];
-    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
-    const videoBuffer = Buffer.from(videoResponse.data, 'base64');
-    res.end(videoBuffer);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred');
-  }
-});
-
 module.exports = router;*/
+
