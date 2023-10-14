43cd471add1580da925c0f1364448fa8abafd5f7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 14:33:13 -0600

    Create a-panties.js

---
+++ routes/a-panties.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const { RandomAgresivo } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/panties.json');
+    const data = response.data;
+    const randomIndex = RandomAgresivo(0, data.length - 1);
+    const imageUrl = data[randomIndex];
+    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(imageBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+module.exports = router;
