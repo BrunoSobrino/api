6d7f12e2295a46ab7e64f994a19d02ac56cb0b4e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 10:43:49 -0600

    Update videoxxx.js

--- routes/videoxxx.js
+++ routes/videoxxx.js
@@ -1,40 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const { RandomAgresivo, getFileName } = require('./func/functions');
 
 router.get('/', async (req, res) => {
   try {
-    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
-    const data = response.data;
-    const randomIndex = Math.floor(data.length * Math.random());
-    const videoUrl = data[randomIndex];
-    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
-    const videoBuffer = Buffer.from(videoResponse.data, 'binary');
-    const tmpDirectory = path.join(__dirname, '..', 'tmp');
-    if (!fs.existsSync(tmpDirectory)) {
-      fs.mkdirSync(tmpDirectory);
-    }
-    let counter = 0;
-    let videoFileName = 'video.mp4';
-    let videoFilePath = path.join(tmpDirectory, videoFileName);
-    while (fs.existsSync(videoFilePath)) {
-      counter++;
-      videoFileName = `video_${counter}.mp4`;
-      videoFilePath = path.join(tmpDirectory, videoFileName);
-    }
-    fs.writeFileSync(videoFilePath, videoBuffer);
     res.setHeader('Content-Type', 'video/mp4');
     res.setHeader('Cache-Control', 'no-store, max-age=0');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
-    if (fs.existsSync(videoFilePath)) {
-      res.sendFile(videoFileName, { root: tmpDirectory });
-    } else {
-      console.error('El archivo no existe en la ubicación especificada.');
-      res.status(404).send('File not found');
-    }
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
+    const data = response.data;
+    const randomIndex = RandomAgresivo(0, data.length - 1);
+    const videoUrl = data[randomIndex];
+    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    res.range({
+      length: videoResponse.headers['content-length'], 
+      accept: req.headers.range,
+    });
+    const tmpFileName = getFileName('videoxxx.mp4', './././tmp'); 
+    const tmpFilePath = path.join(__dirname, '..', 'tmp', tmpFileName);
+    fs.writeFileSync(tmpFilePath, Buffer.from(videoResponse.data, 'base64'));
+    res.sendFile(tmpFileName, { root: path.join(__dirname, '..', 'tmp') });
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred');
@@ -42,3 +31,31 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+
+/*const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const { RandomAgresivo, getFileName } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
+    const data = response.data;
+    const randomIndex = RandomAgresivo(0, data.length - 1);
+    const videoUrl = data[randomIndex];
+    const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    const videoBuffer = Buffer.from(videoResponse.data, 'base64');
+    res.end(videoBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+module.exports = router;*/
