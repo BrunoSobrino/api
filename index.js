4f5989b1460deb2a1c90b5df1545c2d32b29f982
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 09:16:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require('path');
 const { execSync } = require('child_process');
 const axios = require('axios');
 const favicon = require('serve-favicon');
+const range = require('express-range');
 let totalRequests = 0;
 
 var allowedOrigins = ['https://api-brunosobrino.onrender.com', 'https://api.boxmine.xyz', 'http://prem-n1.zipponodes.com:40016', 'https://api.brunosobrino.repl.co'];
@@ -72,6 +73,8 @@ const getUptime = () => {
   const seconds = uptimeInSeconds % 60;
   return `${hours} horas, ${minutes} minutos, ${seconds} segundos`;
 };
+
+app.use(range({ accept: 'bytes' }));
 
 // Subpaginas y usos
 
