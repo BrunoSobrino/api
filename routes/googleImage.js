462586a51c60a53a461704351ef2e703ed46defc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 15:17:07 -0600

    Update googleImage.js

--- routes/googleImage.js
+++ routes/googleImage.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
     const randomIndex = RandomAgresivo(0, imagess.length - 1)
     const image = imagess[randomIndex]
     console.log(image)
-    const imageBuffer = Buffer.from(image)
+    const imageBuffer = Buffer.from(image, 'base64')
     res.end(imageBuffer);
   } catch (error) {
     res.sendFile(path.join(__dirname, '../public/500.html'));
