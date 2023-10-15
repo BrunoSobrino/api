2c0e4ec837b135de87e04cec2d228e6c7108efee
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 23:08:11 -0600

    Update anime-apis.js

--- routes/anime-apis.js
+++ routes/anime-apis.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const path = require('path');
 const { RandomAgresivo } = require('./func/functions');
 
 router.get('/akira', async (req, res) => {
