f44e0e57ceb51552fc1291f440b774a16a1d99d3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 13:45:16 -0600

    Update wallpaper-apis.js

--- routes/wallpaper-apis.js
+++ routes/wallpaper-apis.js
@@ -135,4 +135,218 @@ router.get('/wpmountain', async (req, res) => {
   }
 });
 
+router.get('/pubg', async (req, res) => {
+  try {
+    const pug = ['pubg', 'playerunknowns battlegrounds', 'pubg mobile'];
+    const pug2 = RandomAgresivo(0, pug.length - 1);    
+    const response = await wallpaper(pug2);
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/wpgaming', async (req, res) => {
+  try {
+    const ga = ['gaming', 'gamers', 'video game'];
+    const ga2 = RandomAgresivo(0, ga.length - 1);    
+    const response = await wallpaper(ga2);
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/aesthetic', async (req, res) => {
+  try {
+    const response = await wallpaper('aesthetic');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/pentol', async (req, res) => {
+  try {
+    const response = await wallpaper('milk y mocha');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/cartoon', async (req, res) => {
+  try {
+    const response = await wallpaper('cartoon network');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/cyberspace', async (req, res) => {
+  try {
+    const response = await wallpaper('cyberspace');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/technology', async (req, res) => {
+  try {
+    const response = await wallpaper('technology');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/doraemon', async (req, res) => {
+  try {
+    const response = await wallpaper('doraemon');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/hacker', async (req, res) => {
+  try {
+    const response = await wallpaper('hacker');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/planet', async (req, res) => {
+  try {
+    const response = await wallpaper('planet');
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, response.length - 1);
+      imageUrl = response[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl.image[0], { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
 module.exports = router;
