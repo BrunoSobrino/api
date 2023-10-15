f4f891b94a8798de6471bce7e9b9f0f30a0503ef
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 11:50:00 -0600

    Update functions.js

--- routes/func/functions.js
+++ routes/func/functions.js
@@ -4,28 +4,6 @@ const path = require('path');
 const cheerio = require('cheerio');
 const { fromBuffer  } = require('file-type');
 
-/*async function lyrics(search) {
-  const searchUrl = `https://www.musixmatch.com/search/${search}`;
-  const searchResponse = await axios.get(searchUrl);
-  const searchHtml = searchResponse.data;
-  const $ = cheerio.load(searchHtml);
-  const link = $('div.media-card-body > div > h2').find('a').attr('href');
-  const lyricsUrl = `https://www.musixmatch.com${link}`;
-  const lyricsResponse = await axios.get(lyricsUrl);
-  const lyricsHtml = lyricsResponse.data;
-  const $$ = cheerio.load(lyricsHtml);
-  const thumb = $$('div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div').find('img').attr('src');
-  const lyrics1 = $$('div.col-sm-10.col-md-8.col-ml-6.col-lg-6 > div.mxm-lyrics > span > p > span').text().trim();
-  const lyrics2 = $$('div.col-sm-10.col-md-8.col-ml-6.col-lg-6 > div.mxm-lyrics > span > div > p > span').text().trim();
-  const title = $$('#site > div > div > div > main > div > div > div.mxm-track-banner.top > div > div > div').find('div.col-sm-10.col-md-8.col-ml-9.col-lg-9.static-position > div.track-title-header > div.mxm-track-title > h1').text().trim().replace('Lyrics','')
-  const artist = $$('#site > div > div > div > main > div > div > div > div > div > div > div> div > div > h2 > span').text().trim();
-  if (!thumb || (!lyrics1 && !lyrics2)) {
-    throw new Error('No se encontraron letras para la canción');
-  }
-  const lyrics = `${lyrics1}\n${lyrics2}`;
-  return { thumb: `https:${thumb}`, lyrics, title, artist };
-}*/
-
 async function lyrics(search) {
   const searchUrl = `https://www.musixmatch.com/search/${search}`;
   const searchResponse = await axios.get(searchUrl);
@@ -44,10 +22,9 @@ async function lyrics(search) {
   if (!thumb || (!lyrics1 && !lyrics2)) {
     throw new Error('No se encontraron letras para la canción');
   }
-  const lyrics = `${lyrics1}\n${lyrics2}`;
-  return { thumb: `https:${thumb}`, lyrics, title, artist };
+  const lyrics = `${lyrics1}\n\n\n\n${lyrics2}`;
+  return { status: true, titulo: title, artista: artist, imagen: thumb: `https:${thumb}`, letra: lyrics };
 }
-
 
 function RandomAgresivo(min, max) {
   if (typeof RandomAgresivo.seen === 'undefined') {
