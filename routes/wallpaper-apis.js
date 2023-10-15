8f3b7fefa65816e64bf0172d669011bab2b57f4e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 13:22:45 -0600

    Create wallpaper-apis.js

---
+++ routes/wallpaper-apis.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const path = require('path');
+const { RandomAgresivo } = require('./func/functions');
+
+router.get('/wprandom', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/wprandom.json');
+    const data = response.data;
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/cristianoronaldo', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/CristianoRonaldo.json');
+    const data = response.data;
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/messi', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/Messi.json');
+    const data = response.data;
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/itzy', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/itzy.json');
+    const data = response.data;
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/navidad', async (req, res) => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/navidad.json');
+    const data = response.data;
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+module.exports = router;
