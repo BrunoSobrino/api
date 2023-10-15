2a454eed7f5ae180f1c3dc2bab7a09863b240ae8
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 11:22:51 -0600

    Update ytmp4_2.js

--- routes/ytmp4_2.js
+++ routes/ytmp4_2.js
@@ -5,8 +5,18 @@ const path = require('path');
 const YT = require('./func/YT_mp3_mp4'); 
 
 router.get('/', async (req, res) => {
+  const link = req.query.url; 
   try {
-    const link = req.query.url; 
+    if (!link) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar la URL de video de YouTube'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }    
     const videoData = await YT.mp4_2(link); 
     const videoBuffer = Buffer.from(videoData.buffer); 
     let fileName = `video_${Date.now()}.mp4`;
@@ -20,8 +30,7 @@ router.get('/', async (req, res) => {
     fs.writeFileSync(`./tmp/${fileName}`, videoBuffer);
     res.sendFile(fileName, { root: './tmp' });
   } catch (error) {
-    console.error('Ocurrió un error al procesar la solicitud:', error);
-    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+    res.sendFile(path.join(__dirname, '../public/500.html'));
   }
 });
 
