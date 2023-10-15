7f70897f9589e93acb0e6946393ce91fcddb9d67
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 11:13:01 -0600

    Update ttimg.js

--- routes/ttimg.js
+++ routes/ttimg.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const ttimg = require('./func/ttimg');
 
 router.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     const match_url = req.query.url;
     try {
-        const result = await ttimg(match_url);
-        res.send(JSON.stringify(result, null, 4));
-    } catch (error) {
-        if (!error.response) {
-            res.json({ error: 'An error occurred' });
-        } else {
-            res.json({ error: 'An error occurred' });
-        }
-    }
+    if (!match_url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar la URL del videoimagenes de tiktok.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;      
+    }        
+    const result = await ttimg(match_url);
+    res.send(JSON.stringify(result, null, 4));
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
 });
 
 module.exports = router;
