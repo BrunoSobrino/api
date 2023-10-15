e150897930c4b641f657fdeec0b9705668af7794
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 14:59:19 -0600

    Create googleImage.js

---
+++ routes/googleImage.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const { googleImage } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  const texto = req.query.text;
+  try {
+    if (!texto) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar el texto para buscar la imagen.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }    
+    const image = await googleImage(match_url);
+    console.log(image)
+    const imageBuffer = Buffer.from(image)
+    res.end(imageBuffer);
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+module.exports = router;
