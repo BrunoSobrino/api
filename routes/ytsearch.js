a98b3bcafdb188e26816ac125d2c98dac04f8ba0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 11:24:20 -0600

    Update ytsearch.js

--- routes/ytsearch.js
+++ routes/ytsearch.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const { ytsearch } = require('./func/ytsearch');
 
 router.get('/', async (req, res) => {
   const searchText = req.query.text;
   try {
     if (!searchText) {
-      throw new Error('Texto de búsqueda no especificado');
-    }
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar un texto para hacer la busqueda de vieos en YouTube.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }    
     const results = await ytsearch(searchText);
     const formattedResults = JSON.stringify(results, null, 2);
     res.setHeader('Content-Type', 'application/json');
     res.send(formattedResults);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error en la búsqueda de YouTube' });
+    res.sendFile(path.join(__dirname, '../public/500.html'));
   }
 });
 
