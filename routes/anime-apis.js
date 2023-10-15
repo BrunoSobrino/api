b55278a8dde114caa1901edc4e383502905c2b2e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 23:29:34 -0600

    Update anime-apis.js

--- routes/anime-apis.js
+++ routes/anime-apis.js
@@ -4,6 +4,31 @@ const axios = require('axios');
 const path = require('path');
 const { RandomAgresivo } = require('./func/functions');
 
+router.get('/lolivid', async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/videoxxx.json');
+    const data = response.data;
+    let videoUrl = null;
+    while (!videoUrl) {
+      const randomIndex = RandomAgresivo(0, data.length - 1);
+      videoUrl = data[randomIndex];
+      try {
+        const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+        const videoBuffer = Buffer.from(videoResponse.data, 'base64');
+        res.send(videoBuffer);
+      } catch (error) {
+        videoUrl = null;
+      }
+    }
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
 router.get('/akira', async (req, res) => {
   try {
     const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/anime-akira.json');
