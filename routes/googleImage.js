f746ef46ac2019099b01c812f0649f0748aac444
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 15:11:20 -0600

    Update googleImage.js

--- routes/googleImage.js
+++ routes/googleImage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { googleImage, RandomAgresivo } = require('./func/functions');
+const { googleImage } = require('./func/functions');
 
 router.get('/', async (req, res) => {
   const texto = req.query.text;
