dfa971815a43983fa8f6a6ccc94c9b2591089ade
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 20:19:06 -0600

    Update anime-apis.js

--- routes/anime-apis.js
+++ routes/anime-apis.js
@@ -20,17 +20,24 @@ router.get('/akira', async (req, res) => {
 });
 
 router.get('/akiyama', async (req, res) => {
+  let imageUrl = null;
+  const jsonUrl = 'https://raw.githubusercontent.com/BrunoSobrino/api/main/data/anime-akiyama.json';
   try {
-    const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/anime-akiyama.json');
+    const response = await axios.get(jsonUrl);
     const data = response.data;
-    const randomIndex = RandomAgresivo(0, data.length - 1);
-    const imageUrl = data[randomIndex];
-    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.send(imageBuffer);
+    while (!imageUrl) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
   } catch (error) {
-    console.error(error);
     res.status(500).send('An error occurred');
   }
 });
