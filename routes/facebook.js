6f50dce8160977fa90b8f0382fd38814c9262d26
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 11:03:57 -0600

    Update facebook.js

--- routes/facebook.js
+++ routes/facebook.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const { facebookdlfunc } = require('./func/facebook'); 
 
 router.get('/', async (req, res) => {
   const url = req.query.url;
   try {
+    if (!url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar la URL del video de facebook.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;      
+    }
     const results = await facebookdlfunc(url);
       const formattedResults = JSON.stringify(results, null, 2);
       res.setHeader('Content-Type', 'application/json');
       res.send(formattedResults);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error en la descarga de contenido de Facebook' });
+    res.sendFile(path.join(__dirname, '../public/500.html'));
   }
 });
 
