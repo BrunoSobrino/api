a1dcbdd82ab800ea0a9929f69c2dc8fdd39ab3db
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-14 20:21:53 -0600

    Update anime-apis.js

--- routes/anime-apis.js
+++ routes/anime-apis.js
@@ -7,14 +7,20 @@ router.get('/akira', async (req, res) => {
   try {
     const response = await axios.get('https://raw.githubusercontent.com/BrunoSobrino/api/main/data/anime-akira.json');
     const data = response.data;
-    const randomIndex = RandomAgresivo(0, data.length - 1);
-    const imageUrl = data[randomIndex];
-    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.send(imageBuffer);
+    let imageUrl = null;
+    while (!imageUrl) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      imageUrl = data[randomIndex];
+      try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(imageBuffer);
+      } catch (error) {
+        imageUrl = null;
+      }
+    }
   } catch (error) {
-    console.error(error);
     res.status(500).send('An error occurred');
   }
 });
