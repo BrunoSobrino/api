1837ee4407853708686ccb2e050f3e38e46a5e50
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-15 18:54:56 -0600

    Update stickersearch.js

--- routes/stickersearch.js
+++ routes/stickersearch.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { pinterest } = require('./func/functions');
+const { stickersearch } = require('./func/functions');
 
 router.get('/', async (req, res) => {
-  const texto = req.query.username; 
+  const texto = req.query.text; 
   try {
     if (!texto) {
       const errorResponse = {
@@ -16,8 +16,8 @@ router.get('/', async (req, res) => {
       res.send(formattedResults_e);
       return;
     }        
-    const igStalkss = await pinterest(texto);
-    const formattedResults = JSON.stringify(igStalkss, null, 2);
+    const stickers = await stickersearch(texto);
+    const formattedResults = JSON.stringify(stickers, null, 2);
     res.setHeader('Content-Type', 'application/json');
     res.send(formattedResults);
   } catch (error) {
