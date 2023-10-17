9f158ac298836461a831429b644b6185a8eea4c5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-17 12:26:47 -0600

    Update maker-apis.js

--- routes/maker-apis.js
+++ routes/maker-apis.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const axios = require('axios');
-const { ttp } = require('./func/functions');
+const { maker, ttp } = require('./func/functions');
 
 router.get('/attp', async (req, res) => {
   const texto = req.query.text; 
@@ -19,6 +19,30 @@ router.get('/attp', async (req, res) => {
     }        
     const ttpst = await ttp(texto);
     const imageResponse = await axios.get(ttpst.resultado, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(imageBuffer);
+  } catch (error) {
+    console.log(error)
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/textpro/deep-sea-metal', async (req, res) => {
+  const texto = req.query.text; 
+  try {
+    if (!texto) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar un texto para el logo.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }        
+    const resss = await maker('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', [texto])
+    const imageResponse = await axios.get(resss.image, { responseType: 'arraybuffer' });
     const imageBuffer = Buffer.from(imageResponse.data, 'binary');
     res.setHeader('Content-Type', 'image/jpeg');
     res.send(imageBuffer);
