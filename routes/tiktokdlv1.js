feba0a37fbf7fcbf650bb95be87b6611d1a03c56
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-20 22:56:20 -0600

    Create tiktokdlv1.js

---
+++ routes/tiktokdlv1.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { tiktokv2 } = require('./func/tiktokdl');
+
+router.get('/', async (req, res) => {
+  const url = req.query.url; 
+  try {
+    if (!url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes ingresar el link de un video o imagen de tiktok.'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }        
+    const pint = await tiktokv2(url);
+    const formattedResults = JSON.stringify(pint, null, 2);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(formattedResults);
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+module.exports = router;
