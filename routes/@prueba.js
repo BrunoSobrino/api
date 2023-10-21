470d985bbfceb439aff20b43a8ce1a350fb2b646
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-21 00:08:40 -0600

    Update @prueba.js

--- routes/@prueba.js
+++ routes/@prueba.js
@@ -3,41 +3,47 @@ const router = express.Router();
 const path = require('path');
 const canvacard = require("canvacard");
 
-router.get('/', async (req, res) => {
-  const username = req.query.username 
-  const subtitulo = req.query.subtitulo   
-  const img = req.query.ppuser;
+router.get('/welcome', async (req, res) => {
+  const username = req.query.username;
+  const groupname = req.query.groupname;
+  const groupicon = req.query.groupicon;
+  const membercount = req.query.membercount;
+  const profile = req.query.profile;
   const background = req.query.background;
-  if (!img || !background || !username || !subtitulo) {
+
+  if (!username || !groupname || !groupicon || !membercount || !profile || !background) {
     const errorResponse = {
       status: false,
-      message: 'Debes proporcionar la URL de la imagen de perfil y el fondo, asi como el nombre de usuario y un subtitulo.',
-      example: 'api/maker/canvas/welcome?username=shadow&subtitulo=bienvenido%20al%20grupo&ppuser=https://telegra.ph/file/24fa902ead26340f3df2c.png&background=https://static.videezy.com/system/resources/thumbnails/000/040/439/original/Comp-8.jpg'
+      message: 'Debes proporcionar los parámetros necesarios: username, groupname, groupicon, membercount, profile y background.',
+      example: 'api/maker/canvas/welcome?username=shadow&groupname=MiGrupo&groupicon=https://example.com/groupicon.png&membercount=100&profile=https://example.com/user.png&background=https://example.com/background.jpg'
     };
     const formattedResults_e = JSON.stringify(errorResponse, null, 2);
     res.setHeader('Content-Type', 'application/json');
     res.status(400).send(formattedResults_e);
     return;
   }
-  try {  
-  const welcomer = new canvacard.Welcomer()
-    .setAvatar(img)
-    .setBackground('IMAGE', background)
-    .setTitulo("WELCOME")
-    .setSubtitulo(subtitulo)
-    .setTitulo(username)
-    .setSubtitulo(subtitulo)
-    .setColorTitulo("#FFFFFF")
-    .setColorSubtitulo("#5865f2")
-    .setColorCircle("#FFFFFF")
-    .setColorOverlay("rgba(0, 0, 0, 0.5)")
-    .setOpacityOverlay("0.4");
-    const imageData = await welcomer.build();
+
+  try {
+    const imgBuffer = await welcome(req.query);
+
     res.contentType('image/png');
-    res.send(imageData);
+    res.send(imgBuffer);
   } catch (error) {
     res.sendFile(path.join(__dirname, '../public/500.html'));
   }
 });
+
+async function welcome(params) {
+  const img = await new canvacard.Welcome()
+    .setUsername(params.username)
+    .setGuildName(params.groupname)
+    .setGuildIcon(params.groupicon)
+    .setMemberCount(params.membercount)
+    .setAvatar(params.profile)
+    .setBackground(params.background)
+    .toAttachment();
+  const imgBuffer = await img.toBuffer();
+  return imgBuffer;
+}
 
 module.exports = router;
