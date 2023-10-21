e62696f616dbc8c7cdb361f038eb8c0268bf1a62
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-20 23:26:53 -0600

    Update @prueba.js

--- routes/@prueba.js
+++ routes/@prueba.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { tiktokv2 } = require('./func/tiktokdl');
+const canvacard = require("canvacard");
 
 router.get('/', async (req, res) => {
-  const url = req.query.url; 
-  try {
-    if (!url) {
-      const errorResponse = {
-        status: false,
-        message: 'Debes ingresar el link de un video o imagen de tiktok.'
-      };
-      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(formattedResults_e);
-      return;
-    }        
-    const pint = await tiktokv2(url);
-    const formattedResults = JSON.stringify(pint, null, 2);
+  const img = req.query.img;
+  const background = req.query.background;
+  if (!img || !background) {
+    const errorResponse = {
+      status: false,
+      message: 'Debes proporcionar la URL de la imagen de perfil y el fondo.'
+    };
+    const formattedResults_e = JSON.stringify(errorResponse, null, 2);
     res.setHeader('Content-Type', 'application/json');
-    res.send(formattedResults);
+    res.status(400).send(formattedResults_e);
+    return;
+  }
+  try {  
+  const welcomer = new canvacard.Welcomer()
+    .setAvatar(img)
+    .setBackground('IMAGE', background)
+    .setTitulo("WELCOME")
+    .setSubtitulo("Subtitulo personalizable!")
+    .setTitulo("Titulo personalizable!")
+    .setSubtitulo("Subtitulo personalizable!")
+    .setColorTitulo("#FFFFFF")
+    .setColorSubtitulo("#5865f2")
+    .setColorCircle("#FFFFFF")
+    .setColorOverlay("#000000")
+    .setOpacityOverlay("0.4");
+    const imageData = await welcomer.build();
+    res.contentType('image/png');
+    res.send(imageData);
   } catch (error) {
     res.sendFile(path.join(__dirname, '../public/500.html'));
   }
