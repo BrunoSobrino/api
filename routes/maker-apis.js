35846742674027b9f598577a5bb64c055becc4ab
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-11-05 08:09:25 -0600

    Update maker-apis.js

--- routes/maker-apis.js
+++ routes/maker-apis.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const { maker, ttp } = require('./func/functions');
 const { welCard } = require("@delirius/welcard");
 const { drawCard, LinearGradient } = require('discord-welcome-card');
+const knights = require("@clayzaaubert/canvix");
 
 /* ------------{ canvas }------------ */
 
@@ -174,6 +175,42 @@ router.get('/canvas/welcome4', async (req, res) => {
     });
     res.contentType('image/png');
     res.send(image);
+  } catch (error) {
+    console.log(error)
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+router.get('/canvas/welcome5', async (req, res) => {
+  const username = req.query.username;
+  const groupname = req.query.groupname;
+  const membercount = req.query.membercount;
+  const profile = req.query.profile;
+  const groupicon = req.query.groupicon
+  const background = req.query.background;
+  if (!username || !groupname || !profile || !membercount || !background || !groupicon) {
+    const errorResponse = {
+      status: false,
+      message: 'Debes proporcionar los parámetros necesarios: username, groupname, groupicon, membercount, profile y background.',
+      example: 'api/maker/canvas/welcome5?username=bruno&groupname=api%20empire&membercount=12&groupicon=https://telegra.ph/file/4cc51944c9130560fb4a9.jpg&profile=https://github.com/BrunoSobrino.png&background=https://telegra.ph/file/82d079999da723cc80899.png'
+    };
+    const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(400).send(formattedResults_e);
+    return;
+  }
+  try {
+  const image = await new knights.Welcome()
+      .setUsername(username)
+      .setGuildName(groupname)
+      .setGuildIcon(groupicon)
+      .setMemberCount(membercount)
+      .setAvatar(profile)
+      .setBackground(background)
+      .toAttachment();
+    const data = image.toBuffer();
+    res.contentType('image/png');
+    res.send(data);
   } catch (error) {
     console.log(error)
     res.sendFile(path.join(__dirname, '../public/500.html'));
