1cd35a7f5be7d2c92e97ea538f222644a0188df0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-03 21:29:56 -0600

    Create ytdl3.js

---
+++ routes/func/ytdl3.js
@@ -0,0 +1,35 @@
+const { ytmp3v2, ytmp4 } = require('@ruhend/scraper');
+
+async function obtenerInformacionYoutube(url) {
+    try {
+        const { title: audioTitle, audio } = await ytmp3v2(url);
+        const { title: videoTitle, video, quality, thumbnail, size } = await ytmp4(url);
+        return {
+            status: true,
+            resultado: {
+                ytmp3v2: {
+                    title: audioTitle,
+                    audio
+                },
+                ytmp4: {
+                    title: videoTitle,
+                    video,
+                    quality,
+                    thumbnail,
+                    size
+                }
+            }
+        };
+    } catch (error) {
+        return {
+            status: false,
+            resultado: {
+                error: error.message
+            }
+        };
+    }
+}
+
+module.exports = {
+obtenerInformacionYoutube
+};
