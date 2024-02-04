ddc54abe849848e22b85dba0fd4c31fdf7a13a0b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-03 21:31:03 -0600

    Update ytmp4.js

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const YT = require('./func/YT_mp3_mp4'); 
+const { obtenerInformacionYoutube } = require('./func/ytdl3');
+const { getBuffer } = require('./func/functions');
 
 router.get('/', async (req, res) => {
   const link = req.query.url;
@@ -29,9 +31,25 @@ router.get('/', async (req, res) => {
     }
     fs.renameSync(videoPath, `./tmp/${fileName}`);
     res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'video/mp4' } });
+   } catch {
+      try {
+    const youtubeInfo = await obtenerInformacionYoutube(link);
+      const formattedResults = JSON.stringify(youtubeInfo, null, 2);
+      const video = await getBuffer(youtubeInfo.resultado.ytmp4.video)
+    const videoBuffer = Buffer.from(video); 
+    let fileName = `videomp4_${Date.now()}.mp4`;
+    let fileIndex = 1;
+    while (fs.existsSync(`./tmp/${fileName}`)) {
+      const extension = path.extname(fileName);
+      const baseName = path.basename(fileName, extension);
+      fileName = `${baseName}_${fileIndex}${extension}`;
+      fileIndex++;
+    }
+    fs.writeFileSync(`./tmp/${fileName}`, videoBuffer);
+    res.sendFile(fileName, { root: './tmp' });     
   } catch (error) {
     res.sendFile(path.join(__dirname, '../public/500.html'));
   }
-});
+}});
 
 module.exports = router;
