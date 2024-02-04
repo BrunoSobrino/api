b03de83aa9ddb106fdac4e57d38dfec9f78dbee7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-03 22:03:42 -0600

    Create ytmp3_1.js

---
+++ routes/human/ytmp3_1.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const YT = require('./func/YT_mp3_mp4');
+const { obtenerInformacionYoutube } = require('./func/ytdl3');
+const { getBuffer } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  const match_url = req.query.url;
+  try {
+    if (!match_url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar la URL de video de YouTube'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }    
+
+    const youtubeInfo = await obtenerInformacionYoutube(match_url);
+    const audioBuffer = await getBuffer(youtubeInfo.resultado.ytmp3v2.audio);
+
+    let fileName = `audiomp3_${Date.now()}.mp3`;
+    let fileIndex = 1;
+    while (fs.existsSync(`./tmp/${fileName}`)) {
+      const extension = path.extname(fileName);
+      const baseName = path.basename(fileName, extension);
+      fileName = `${baseName}_${fileIndex}${extension}`;
+      fileIndex++;
+    }
+
+    fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
+
+    // Configurar el encabezado para la descarga del archivo
+    res.setHeader('Content-Disposition', `attachment; filename="${youtubeInfo.resultado.ytmp3v2.title || fileName}"`);
+    res.sendFile(fileName, { root: './tmp' });
+    
+  } catch (error) {  
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+module.exports = router;
