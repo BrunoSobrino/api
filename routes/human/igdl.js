1419f13065c2da017a464aec61a4ac9f0bc061bd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-04 08:25:04 -0600

    Update igdl.js

--- routes/human/igdl.js
+++ routes/human/igdl.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const axios = require('axios');
 const fs = require('fs');
+const archiver = require('archiver');
 const { igdl2 } = require('../func/igdl');
 const { getBuffer } = require('../func/functions');
 
@@ -19,7 +20,60 @@ router.get('/', async (req, res) => {
       res.send(formattedResults_e);
       return;
     }
+
     const results = await igdl2(url);
+
+    if (results.data.length > 1) {
+      const archivosPromises = results.data.map(async (archivo, index) => {
+        const fileType = archivo.type;
+        const fileUrl = archivo.url_download;
+
+        // Realizar la solicitud HTTP a la URL de descarga de Instagram
+        const archivoBuffer = await getBuffer(fileUrl);
+
+        const extension = fileType === 'video' ? 'mp4' : 'jpg';
+        let fileName = `instagram_${Date.now()}_${index + 1}.${extension}`;
+
+        let fileIndex = 1;
+        while (fs.existsSync(`./tmp/${fileName}`)) {
+          const baseName = path.basename(fileName, extension);
+          fileName = `${baseName}_${fileIndex}.${extension}`;
+          fileIndex++;
+        }
+
+        fs.writeFileSync(`./tmp/${fileName}`, archivoBuffer);
+
+        return {
+          fileName,
+          fileType
+        };
+      });
+
+      // Esperar a que se completen todas las descargas
+      const archivosDescargados = await Promise.all(archivosPromises);
+
+      // Crear un archivo ZIP con todos los archivos descargados
+      const zipFileName = `instagram_${Date.now()}.zip`;
+      const zipFilePath = path.join(__dirname, `../../tmp/${zipFileName}`);
+      const output = fs.createWriteStream(zipFilePath);
+      const zip = archiver('zip', { zlib: { level: 9 } });
+
+      output.on('close', () => {
+        res.setHeader('Content-Type', 'application/zip');
+        res.setHeader('Content-Disposition', `attachment; filename="${zipFileName}"`);
+        res.sendFile(zipFilePath);
+      });
+
+      zip.pipe(output);
+
+      // Agregar cada archivo al archivo ZIP
+      archivosDescargados.forEach(({ fileName }) => {
+        zip.file(path.join(__dirname, `../../tmp/${fileName}`), { name: fileName });
+      });
+
+      // Finalizar el archivo ZIP cuando se han agregado todos los archivos
+      zip.finalize();
+    } else {
       const fileType = results.data[0].type;
       const fileUrl = results.data[0].url_download;
       const archivoBuffer = await getBuffer(fileUrl);
@@ -35,7 +89,9 @@ router.get('/', async (req, res) => {
       res.setHeader('Content-Type', fileType === 'video' ? 'video/mp4' : 'image/jpeg');
       res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
       res.send(archivoBuffer);
+    }
   } catch (error) {
+    console.error(`Error al procesar la solicitud: ${error.message}`);
     res.sendFile(path.join(__dirname, '../../public/500.html'));
   }
 });
