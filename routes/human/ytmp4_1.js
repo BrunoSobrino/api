fe8543fa0191ae55ce7a87f2d859f46070072b8b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-03 22:37:41 -0600

    Update ytmp4_1.js

--- routes/human/ytmp4_1.js
+++ routes/human/ytmp4_1.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const YT = require('./func/YT_mp3_mp4');
-const { obtenerInformacionYoutube } = require('./func/ytdl3');
-const { getBuffer } = require('./func/functions');
+const { obtenerInformacionYoutube } = require('../func/ytdl3');
+const { getBuffer } = require('../func/functions');
 
 router.get('/', async (req, res) => {
   const match_url = req.query.url;
@@ -19,10 +18,8 @@ router.get('/', async (req, res) => {
       res.send(formattedResults_e);
       return;
     }    
-
     const youtubeInfo = await obtenerInformacionYoutube(match_url);
     const videoBuffer = await getBuffer(youtubeInfo.resultado.ytmp4.video);
-
     let fileName = `videomp4_${Date.now()}.mp4`;
     let fileIndex = 1;
     while (fs.existsSync(`./tmp/${fileName}`)) {
@@ -31,13 +28,9 @@ router.get('/', async (req, res) => {
       fileName = `${baseName}_${fileIndex}${extension}`;
       fileIndex++;
     }
-
     fs.writeFileSync(`./tmp/${fileName}`, videoBuffer);
-
-    // Configurar el encabezado para la descarga del archivo
-    res.setHeader('Content-Disposition', `attachment; filename="${youtubeInfo.resultado.ytmp4.title || fileName}"`);
+    res.setHeader('Content-Disposition', `attachment; filename="${youtubeInfo.resultado.ytmp4.title || fileName}.mp4"`);
     res.sendFile(fileName, { root: './tmp' });
-    
   } catch (error) {  
     res.sendFile(path.join(__dirname, '../public/500.html'));
   }
