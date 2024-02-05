d3ab1300377abc87cf39efe41255e6644a488907
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-05 06:02:31 -0600

    Update fbdl.js

--- routes/human/fbdl.js
+++ routes/human/fbdl.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const { facebookdlfunc } = require('../func/facebook');
 const { getBuffer } = require('../func/functions');
-const fileType = require('file-type');
 
 router.get('/', async (req, res) => {
   const url = req.query.url;
@@ -19,20 +18,9 @@ router.get('/', async (req, res) => {
       res.send(formattedResults_e);
       return;
     }
-
     const results = await facebookdlfunc(url);
     const fileData = results.resultado.data;
-
-    // Detectar el tipo de archivo utilizando file-type
-    const detectedFileType = fileType(fileData);
-
-    // Determinar si es video o imagen
-    const isVideo = detectedFileType && detectedFileType.mime.includes('video');
-
-    // Nombre personalizado para el archivo de descarga
-    let fileName = isVideo ? `facebook_video_${Date.now()}.mp4` : `facebook_imagen_${Date.now()}.png`;
-
-    // Lógica para cambiar el nombre si ya existe
+    let fileName = `facebook_video_${Date.now()}.mp4`;
     let fileIndex = 1;
     while (fs.existsSync(`./tmp/${fileName}`)) {
       const extension = path.extname(fileName);
@@ -40,17 +28,9 @@ router.get('/', async (req, res) => {
       fileName = `${baseName}_${fileIndex}${extension}`;
       fileIndex++;
     }
-
-    // Convertir el archivo a buffer (si es necesario)
     const fileBuffer = Buffer.isBuffer(fileData) ? fileData : await getBuffer(fileData);
-
-    // Guardar el archivo en el directorio temporal
     fs.writeFileSync(`./tmp/${fileName}`, fileBuffer);
-
-    // Configurar el encabezado para la descarga
     res.attachment(fileName);
-
-    // Enviar el archivo como descarga
     res.send(fileBuffer);
   } catch (error) {
     console.log(error)
