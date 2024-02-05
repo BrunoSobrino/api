273ae1b83145e0b9593efef434c034132b865cb6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-05 09:50:53 -0600

    Create x_twitter.js

---
+++ routes/x_twitter.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { downloadTwitterMedia } = require('./func/functions');
+
+router.get('/', async (req, res) => {
+  const url = req.query.url; 
+  try {
+    if (!url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes ingresar el link de un video o imagen de X (Twitter).'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }        
+    const pint = await downloadTwitterMedia(url);
+    const formattedResults = JSON.stringify(pint, null, 2);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(formattedResults);
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+  }
+});
+
+module.exports = router;
