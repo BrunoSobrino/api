7ceba000691a52dc6c4146a8d2848a77812f3b5c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-10 08:28:17 -0600

    Update spotify.js

--- routes/func/spotify.js
+++ routes/func/spotify.js
@@ -175,6 +175,7 @@ async function spotifyDownload(input) {
     if (!isSpotifyUrl) return { status: false, message: 'El link ingresado no es de algun album de spotify.' };
    try {
         const response = await downloadAlbum(input);
+        console.loh(response)
         return response;
     } catch (error) {
         return { status: false, error: error.message };
