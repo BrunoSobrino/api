59f2e99f673b3412560da7aa3df71f1568f726c4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-02-10 08:34:37 -0600

    Update spotifydl_album.js

--- routes/spotifydl_album.js
+++ routes/spotifydl_album.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const path = require('path');
 const { spotifyDownload } = require('./func/spotify');
 
-// Función para convertir un array de números en una cadena separada por comas
-function arrayToCommaSeparatedString(array) {
-  return array.join(', ');
-}
-
 router.get('/', async (req, res) => {
   const urll = req.query.url;
   const input = urll;
@@ -15,32 +10,16 @@ router.get('/', async (req, res) => {
     if (!input) {
       const errorResponse = {
         status: false,
-        message: 'Debes especificar la URL de la música o Álbum de Spotify.'
+        message: 'Debes especificar la URL de un Álbum de Spotify.'
       };
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify(errorResponse, null, 2));
       return;      
     }
-
     let spty = await spotifyDownload(input);
-
-    // Convertir audioBuffer en una cadena de números separada por comas
-    let audioBufferString = '';
-    if (spty.audioBuffer) {
-      audioBufferString = arrayToCommaSeparatedString(spty.audioBuffer);
-    } else if (spty.trackList && spty.trackList.length > 0) {
-      const trackAudioBuffers = spty.trackList.map(track => track.audioBuffer).filter(buffer => buffer); // Filter out null or undefined values
-      audioBufferString = arrayToCommaSeparatedString([].concat(...trackAudioBuffers));
-    }
-
-    // Crear un nuevo objeto que contenga todos los datos menos audioBuffer
+    const audioBufferString = (spty.trackList[0].audioBuffer).join(', ');
     const sptyWithoutAudioBuffer = { ...spty };
-    delete sptyWithoutAudioBuffer.audioBuffer;
-    if (sptyWithoutAudioBuffer.trackList) {
-      sptyWithoutAudioBuffer.trackList.forEach(track => delete track.audioBuffer);
-    }
-
-    // Enviar los datos formateados como JSON, incluyendo audioBuffer como una cadena de texto
+    delete sptyWithoutAudioBuffer.trackList[0].audioBuffer;
     res.setHeader('Content-Type', 'application/json');  
     res.send(JSON.stringify({
       ...sptyWithoutAudioBuffer,
