1d2b1c67907695bf92c82f6fa099354ba688dd19
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-05-16 09:58:25 -0600

    Update chatgpt.js

--- routes/func/chatgpt.js
+++ routes/func/chatgpt.js
@@ -14,60 +14,6 @@ async function chatgpt(text, lenguaje = 'es') {
     resultado: "",
   };
   const apiEndpoints = [
-/*    {
-      url: `https://api-fgmods.ddns.net/api/info/openai2?text=${text}&apikey=XlwAnX8d`,
-      processResponse: async (data) => {
-        if (data?.result != 'error' && data?.result != '' && data?.result != undefined && data?.result) {
-          const translatedResult = await translate(data.result, { to: lenguaje, autoCorrect: true });
-          result.resultado = translatedResult.text.trim();
-        }
-      },
-    },
-    {
-      url: `https://vihangayt.me/tools/chatgpt?q=${text}`,
-      processResponse: async (data) => {
-        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
-          let parsedData = '';
-          try {
-            parsedData = unescape(data.data);
-          } catch {
-            parsedData = data.data;
-          }
-          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
-          result.resultado = translatedResult.text.trim();
-        }
-      },
-    },
-    {
-      url: `https://vihangayt.me/tools/chatgpt2?q=${text}`,
-      processResponse: async (data) => {
-        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
-          let parsedData = '';
-          try {
-            parsedData = unescape(data.data);
-          } catch {
-            parsedData = data.data;
-          }
-          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
-          result.resultado = translatedResult.text.trim();
-        }
-      },
-    },
-    {
-      url: `https://vihangayt.me/tools/chatgpt3?q=${text}`,
-      processResponse: async (data) => {
-        if (data?.data != 'error' && data?.data != '' && data?.data != undefined && data?.data) {
-          let parsedData = '';
-          try {
-            parsedData = unescape(data.data);
-          } catch {
-            parsedData = data.data;
-          }
-          const translatedResult = await translate(parsedData.replace(/\\[uU]([0-9A-Fa-f]{4})/g, (match, grp) => String.fromCharCode(parseInt(grp, 16))), { to: lenguaje, autoCorrect: true });
-          result.resultado = translatedResult.text.trim();
-        }
-      },
-    },*/
     {
       url: `https://api.lolhuman.xyz/api/openai?apikey=GataDios&text=${text}&user=apirest`,
       processResponse: async (data) => {
@@ -128,8 +74,7 @@ async function gpt(content, senderName, prompt) {
     status: true,
     resultado: "",
   };
-  
-  /*let url = 'https://c3.a0.chat/v1/chat/gpt/';
+  let url = 'https://c3.a0.chat/v1/chat/gpt/';
   let headers = {
     'Content-Type': 'application/json',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2004J19C Build/RP1A.200720.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.129 Mobile Safari/537.36 WhatsApp/1.2.3',
@@ -157,7 +102,7 @@ async function gpt(content, senderName, prompt) {
   try {
     let ress = await axios.post(url, datos, { headers });
     result.resultado = ress.data
-  } catch {*/
+  } catch {
   try {
     let resultadoApi = await fetch(`https://aemt.me/prompt/gpt?prompt=${prompt}&text=${content}`)
     const resultado_Api = await resultadoApi.json()
@@ -165,13 +110,19 @@ async function gpt(content, senderName, prompt) {
     return result;
   } catch { 
   try {
+    let resultadoApi3 = await fetch(`https://delirios-api-delta.vercel.app/ia/gptprompt?text=${content}&prompt=${prompt}`)
+    const resultado_Api3 = await resultadoApi3.json()
+    result.resultado = resultado_Api3.gpt
+    return result;
+  } catch {
+  try {
     let resultadoApi2 = await fetch(`https://ultimetron.guruapi.tech/gpt4?prompt=${content}`)
     const resultado_Api2 = await resultadoApi2.json()
     result.resultado = resultado_Api2.result.reply
-    return result;
-  } catch (error) {
+    return result;    
+  } catch (error) {    
     return { status: false, error: error.message };
-  }}
+  }}}
 }
 
 module.exports = { chatgpt, gpt };
