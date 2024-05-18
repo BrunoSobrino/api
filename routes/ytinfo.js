f7ba509ecb14672090fcd9ee42691277647461cf
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-05-17 22:26:17 -0600

    Add files via upload

---
+++ routes/ytinfo.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const YT = require('./func/YT_mp3_mp4');
+
+router.get('/', async (req, res) => {
+  const match_url = req.query.url;
+  try {
+    if (!match_url) {
+      const errorResponse = {
+        status: false,
+        message: 'Debes especificar la URL de video de YouTube'
+      };
+      const formattedResults_e = JSON.stringify(errorResponse, null, 2);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResults_e);
+      return;
+    }        
+    const infoaud = await YT.ytinfo2(match_url);
+    const formattedResults = JSON.stringify(infoaud, null, 2);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(formattedResults);    
+  } catch (error) {
+    res.sendFile(path.join(__dirname, '../../public/500.html'));
+  }
+});
+
+module.exports = router;
