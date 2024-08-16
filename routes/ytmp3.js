ba5f78ca25fdf2de009704908b22161eae5f8c0d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-16 16:47:52 -0600

    Update ytmp3.js

--- routes/ytmp3.js
+++ routes/ytmp3.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const YT = require('./func/YT_mp3_mp4');
+const { ytmp33 } = require('./func/yt25');
 const { obtenerInformacionYoutube } = require('./func/ytdl3');
 const { getBuffer } = require('./func/functions');
 
@@ -18,35 +19,57 @@ router.get('/', async (req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.send(formattedResults_e);
       return;
-    }    
-    const audioBuffer = await YT.mp3(match_url);
-    let fileName = `audio_${Date.now()}.mp3`;
-    let fileIndex = 1;
-    while (fs.existsSync(`./tmp/${fileName}`)) {
-      fileName = `audio_${Date.now()}_${fileIndex}.mp3`;
-      fileIndex++;
     }
-    fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
-    res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'audio/mpeg' } });
-  } catch {
+    
+    try {
+      const result = await ytmp33(match_url);
+      if (result.status) {
+        const fileName = `audio_${Date.now()}.mp3`;
+        let fileIndex = 1;
+        while (fs.existsSync(`./tmp/${fileName}`)) {
+          fileName = `audio_${Date.now()}_${fileIndex}.mp3`;
+          fileIndex++;
+        }
+        fs.writeFileSync(`./tmp/${fileName}`, Buffer.from(result.resultados.descargar));
+        res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'audio/mpeg' } });
+      } else {
+        throw new Error(result.error);
+      }
+    } catch (error) {
       try {
-    const youtubeInfo = await obtenerInformacionYoutube(match_url);
-      const formattedResults = JSON.stringify(youtubeInfo, null, 2);
-      const audio = await getBuffer(youtubeInfo.resultado.ytmp3v2.audio)
-    const audioBuffer = Buffer.from(audio); 
-    let fileName = `audiomp3_${Date.now()}.mp3`;
-    let fileIndex = 1;
-    while (fs.existsSync(`./tmp/${fileName}`)) {
-      const extension = path.extname(fileName);
-      const baseName = path.basename(fileName, extension);
-      fileName = `${baseName}_${fileIndex}${extension}`;
-      fileIndex++;
+        const audioBuffer = await YT.mp3(match_url);
+        let fileName = `audio_${Date.now()}.mp3`;
+        let fileIndex = 1;
+        while (fs.existsSync(`./tmp/${fileName}`)) {
+          fileName = `audio_${Date.now()}_${fileIndex}.mp3`;
+          fileIndex++;
+        }
+        fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
+        res.sendFile(fileName, { root: './tmp', headers: { 'Content-Type': 'audio/mpeg' } });
+      } catch {
+        try {
+          const youtubeInfo = await obtenerInformacionYoutube(match_url);
+          const formattedResults = JSON.stringify(youtubeInfo, null, 2);
+          const audio = await getBuffer(youtubeInfo.resultado.ytmp3v2.audio);
+          const audioBuffer = Buffer.from(audio); 
+          let fileName = `audiomp3_${Date.now()}.mp3`;
+          let fileIndex = 1;
+          while (fs.existsSync(`./tmp/${fileName}`)) {
+            const extension = path.extname(fileName);
+            const baseName = path.basename(fileName, extension);
+            fileName = `${baseName}_${fileIndex}${extension}`;
+            fileIndex++;
+          }
+          fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
+          res.sendFile(fileName, { root: './tmp' });
+        } catch (error) {
+          res.sendFile(path.join(__dirname, '../public/500.html'));
+        }
+      }
     }
-    fs.writeFileSync(`./tmp/${fileName}`, audioBuffer);
-    res.sendFile(fileName, { root: './tmp' });
-  } catch (error) {  
+  } catch (error) {
     res.sendFile(path.join(__dirname, '../public/500.html'));
   }
-  }});
+});
 
 module.exports = router;
